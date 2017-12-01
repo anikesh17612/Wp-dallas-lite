2d6c0ccc589c2439662bd312564344ef79658ae1
Siddhartha Agarwal <siddhartha10jun@gmail.com>
2017-12-02 00:47:23 +0530

    include font awesome

--- functions.php
+++ functions.php
@@ -159,6 +159,7 @@ function wp_dallas_lite_scripts() {
 
 	wp_enqueue_script( 'wp_dallas_lite-navigation', get_template_directory_uri() . 'assets/js/navigation.js', array(), '20151215', true );
 	wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/assets/css/bootstrap.min.css' );
+	wp_enqueue_style( 'font-awesome', get_template_directory_uri() . '/assets/css/font-awesome.min.css' );
 	wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/assets/js/bootstrap.min.js', array(), '3.0.0', true );
 	wp_enqueue_script( 'wp_dallas_lite-skip-link-focus-fix', get_template_directory_uri() . 'assets/js/skip-link-focus-fix.js', array(), '20151215', true );
 	
