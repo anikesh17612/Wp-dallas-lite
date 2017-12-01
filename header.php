c4dc10696a83f9a678afdeb3cdec8fa602e0c468
Siddhartha Agarwal <siddhartha10jun@gmail.com>
2017-12-01 06:20:33 +0530

    add top header menu and social icon div

--- header.php
+++ header.php
@@ -24,6 +24,21 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'wp_dallas_lite' ); ?></a>
 
 	<header id="masthead" class="site-header">
+		<div class="top-header">
+			<div class="top-menu">
+				<?php
+					wp_nav_menu( array(
+						'theme_location' => 'menu-2',
+						'menu_id'        => 'Top-menu',
+					) );
+				?>
+				
+			</div>
+			<div class="social-icon">
+				
+			
+			</div>
+		</div><!-- top-header -->
 		<div class="site-branding">
 			<?php
 			the_custom_logo();
@@ -39,9 +54,9 @@
 				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
 			<?php
 			endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
+			
+			
+			<nav id="site-navigation" class="main-navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'wp_dallas_lite' ); ?></button>
 			<?php
 				wp_nav_menu( array(
@@ -50,6 +65,9 @@
 				) );
 			?>
 		</nav><!-- #site-navigation -->
+		</div><!-- .site-branding -->
+
+		
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
