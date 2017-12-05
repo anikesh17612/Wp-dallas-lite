413ddc63ff47a1ec94e5f210f57176a2f1e3bbfa
Siddhartha Agarwal <siddhartha10jun@gmail.com>
2017-12-05 07:12:27 +0530

    include google font file

--- functions.php
+++ functions.php
@@ -236,4 +236,7 @@ if ( class_exists( 'WooCommerce' ) ) {
 }
 
 require_once( get_template_directory()  . '/lib/theme-register-function.php'); 
+require_once( get_template_directory()  . '/lib/googlefonts.php'); 
+
+
 
