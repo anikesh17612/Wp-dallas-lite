4a09954c6552e0d11c325df38a84995beb158871
Siddhartha Agarwal <siddhartha10jun@gmail.com>
2017-12-06 03:08:46 +0530

    layout complete

--- 404.php
+++ 404.php
@@ -14,41 +14,15 @@ get_header(); ?>
 
 			<section class="error-404 not-found">
 				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'wp_dallas_lite' ); ?></h1>
+					
+					<h1 class="page-title"><?php echo get_theme_mod('404pageTitle'); ?></h1>
+					<img src="<?php echo get_theme_mod( 'pageLogoImage' ); ?>" alt="404-logo">
+					
 				</header><!-- .page-header -->
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'wp_dallas_lite' ); ?></p>
-
-					<?php
-						get_search_form();
-
-						the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'wp_dallas_lite' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'wp_dallas_lite' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
+				<div class="page-content" style="background-image:url(<?php echo get_theme_mod('pageBackgroundImage')?>)">
+					<p><?php echo get_theme_mod('404pageDescription'); ?></p>
+					<p><a href="/home"><?php echo get_theme_mod('404buttonText'); ?></a></p>
 
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
