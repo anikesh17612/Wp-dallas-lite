e0ae63569815a6ddfebd5210eee22e3645d12b3e
Ashish Singh <ashish.singh@joomdev.com>
2017-12-06 02:35:23 +0530

    display content on basis on blog settings

--- template-parts/content.php
+++ template-parts/content.php
@@ -27,10 +27,35 @@
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
-		<?php
+				<?php if ( is_single() ) { ?>
+					<div class="single-entry-content">
+						<?php the_content();?>
+					</div>
+				<?php } else {
+					 if ( get_theme_mod( 'enableExcerpt', true ) ) { 
+						if ( get_theme_mod( 'excerptCharacterLimit', 330 ) ) {
+							$textlimit = get_theme_mod( 'excerptCharacterLimit', 330 );
+							echo wpdallas_excerpt_max_charlength($textlimit);
+						} else {
+							echo  the_content();
+						}
+						if ( get_theme_mod( 'enableBlogReadmore', true ) ) { 
+							if ( get_theme_mod( 'continueReading', 'Continue reading...' ) ) {
+								$continue = esc_html( get_theme_mod( 'continueReading', 'Continue reading...' ) );
+								echo '<div class="meta-content-limit"></div>';
+								echo '<p class="wrap-btn-style"><a class="btn btn-style" href="'.get_permalink().'">'. $continue .'</a></p>';
+
+							} 
+						}  
+					} 
+				} 
+	 
+			/*
+			//This is default comment for wpdallas Blog Excerpts setting implimentaion   	
+
 			the_content( sprintf(
 				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
+				 //translators: %s: Name of current post. Only visible to screen readers 
 					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'wp_dallas_lite' ),
 					array(
 						'span' => array(
@@ -39,7 +64,7 @@
 					)
 				),
 				get_the_title()
-			) );
+			) ); */
 
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'wp_dallas_lite' ),
