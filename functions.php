ad0df843a6c059090511e2e09d29ee459e12d4b3
Ashish Singh <ashish.singh@joomdev.com>
2017-12-06 02:34:37 +0530

    Added function for character limit

--- functions.php
+++ functions.php
@@ -199,7 +199,29 @@ function load_posts_by_ajax_callback() {
 }
 add_action('wp_ajax_load_posts_by_ajax', 'load_posts_by_ajax_callback');
 
+/*-----------------------------------------------------
+ * 				Custom Excerpt Length
+ *----------------------------------------------------*/
+if(!function_exists('wpdallas_excerpt_max_charlength')):
+	function wpdallas_excerpt_max_charlength($charlength) {
+		$excerpt = get_the_excerpt();
+		$charlength++;
 
+		if ( mb_strlen( $excerpt ) > $charlength ) {
+			$subex = mb_substr( $excerpt, 0, $charlength - 5 );
+			$exwords = explode( ' ', $subex );
+			$excut = - ( mb_strlen( $exwords[ count( $exwords ) - 1 ] ) );
+			if ( $excut < 0 ) {
+				return mb_substr( $subex, 0, $excut );
+			} else {
+				return $subex;
+			}
+
+		} else {
+			return $excerpt;
+		}
+	}
+endif;
 
 /**
  * Implement the Custom Header feature.
