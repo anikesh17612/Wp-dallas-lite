424143d8f54a14c5632b056565f2dea766b81308
Chandan Deep <chandanndeep@gmail.com>
2017-12-08 21:52:28 +0530

    Main menu

--- header.php
+++ header.php
@@ -78,8 +78,7 @@
 					<?php
 						wp_nav_menu( array(
 							'theme_location' => 'menu-1',
-							'menu_id'        => 'primary-menu',
-							'menu_class'	 => 'nav menu'
+							'menu_id'        => 'primary-menu'
 						) );
 					?>
 				</nav>
