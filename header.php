a82780148f162a03cf7f95f668755bb79777116b
Chandan Deep <chandanndeep@gmail.com>
2017-12-08 19:56:05 +0530

    Mobile Icon change

--- header.php
+++ header.php
@@ -72,14 +72,14 @@
 				
 				
 				<nav id="site-navigation" class="main-navigation">
-				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'wp_dallas_lite' ); ?></button>
-				<?php
-					wp_nav_menu( array(
-						'theme_location' => 'menu-1',
-						'menu_id'        => 'primary-menu',
-					) );
-				?>
-			</nav><!-- #site-navigation -->
+					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menus', 'wp_dallas_lite' ); ?></button>
+					<?php
+						wp_nav_menu( array(
+							'theme_location' => 'menu-1',
+							'menu_id'        => 'primary-menu',
+						) );
+					?>
+				</nav><!-- #site-navigation -->
 			</div><!-- .site-branding -->
 
 		</div>
