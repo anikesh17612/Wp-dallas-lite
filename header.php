e50b6e961502b09bb330d8e344d5a4d9c0dc72c5
Chandan Deep <chandanndeep@gmail.com>
2017-12-08 12:26:09 +0530

    Add Container Div
    Add Container Div for Full-width and Box Layout.

--- header.php
+++ header.php
@@ -23,62 +23,66 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'wp_dallas_lite' ); ?></a>
 	<div class="top-header row">
-			<div class="top-menu col-md-6 col-sm-12 col-xs-12">
+		<div class="container">
+				<div class="top-menu col-md-6 col-sm-12 col-xs-12">
+					<?php
+						wp_nav_menu( array(
+							'theme_location' => 'menu-2',
+							'menu_id'        => 'Top-menu',
+						) );
+					?>
+					
+				</div>
+				<div class="social-icon col-md-6 col-sm-12 col-xs-12">
+					
+				<?php echo socialicon();?>
+				</div>
+		</div>
+	</div>
+	<!-- Top Header -->
+	<header id="masthead" class="site-header">
+		<div class="container">
+			<div class="site-branding">
+				<?php
+				if(get_theme_mod('allLogoFavicon') == 'logo-image' || get_theme_mod('allLogoFavicon')==""){?>
+					
+					<?php if(empty(get_theme_mod('uploadLogo'))){?>
+						<div class="wpdal_logo_image"><img src="<?php echo get_template_directory_uri().'/assets/images/logo.png';?>" alt=""></div> 
+					<?php }else{?>
+						<div class="wpdal_logo_image"><img src="<?php echo get_theme_mod('uploadLogo');?>" alt=""></div> 
+					<?php }?>
+				<?php }else if(get_theme_mod('allLogoFavicon')=='logo-text'){?>
+					<div class="wpdal_logo_text"><h1><?php echo get_theme_mod('siteTitle')?></h1></div> 
+					<div class="wpdal_logo_text"><p><?php echo get_theme_mod('tagLine')?></p></div> 
+				<?php }else{
+					the_custom_logo();	
+				}
+				if ( is_front_page() && is_home() ) : ?>
+					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<?php else : ?>
+					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<?php
+				endif;
+
+				$description = get_bloginfo( 'description', 'display' );
+				if ( $description || is_customize_preview() ) : ?>
+					<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+				<?php
+				endif; ?>
+				
+				
+				<nav id="site-navigation" class="main-navigation">
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'wp_dallas_lite' ); ?></button>
 				<?php
 					wp_nav_menu( array(
-						'theme_location' => 'menu-2',
-						'menu_id'        => 'Top-menu',
+						'theme_location' => 'menu-1',
+						'menu_id'        => 'primary-menu',
 					) );
 				?>
-				
-			</div>
-			<div class="social-icon col-md-6 col-sm-12 col-xs-12">
-				
-			<?php echo socialicon();?>
-			</div>
-	</div><!-- Top Header -->
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			if(get_theme_mod('allLogoFavicon') == 'logo-image' || get_theme_mod('allLogoFavicon')==""){?>
-				
-				<?php if(empty(get_theme_mod('uploadLogo'))){?>
-					<div class="wpdal_logo_image"><img src="<?php echo get_template_directory_uri().'/assets/images/logo.png';?>" alt=""></div> 
-				<?php }else{?>
-					<div class="wpdal_logo_image"><img src="<?php echo get_theme_mod('uploadLogo');?>" alt=""></div> 
-				<?php }?>
-			<?php }else if(get_theme_mod('allLogoFavicon')=='logo-text'){?>
-				<div class="wpdal_logo_text"><h1><?php echo get_theme_mod('siteTitle')?></h1></div> 
-				<div class="wpdal_logo_text"><p><?php echo get_theme_mod('tagLine')?></p></div> 
-			<?php }else{
-				the_custom_logo();	
-			}
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
+			</nav><!-- #site-navigation -->
+			</div><!-- .site-branding -->
 
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-			
-			
-			<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'wp_dallas_lite' ); ?></button>
-			<?php
-				wp_nav_menu( array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				) );
-			?>
-		</nav><!-- #site-navigation -->
-		</div><!-- .site-branding -->
-
-		
+		</div>
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
