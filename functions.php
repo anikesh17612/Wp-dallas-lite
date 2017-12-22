442df60b330e6a24e0a02573823daf94a8a7c446
Siddhatha Agarwal <siddhartha10jun@gmail.com>
2017-12-22 23:34:34 +0530

    changes

--- functions.php
+++ functions.php
@@ -191,8 +191,8 @@ function wp_dallas_lite_scripts()
 	wp_enqueue_script('wp_dallas_lite-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/skip-link-focus-fix.js', array(
 		'jquery'
 	) , '20151215', true);
-	wp_enqueue_style('personalblog-style', get_stylesheet_uri()); //
-	wp_add_inline_style('personalblog-style', wp_dallas_lite_css_generator()); //
+	wp_enqueue_style('wpdallaslite-style', get_stylesheet_uri()); //
+	wp_add_inline_style('wpdallaslite-style', wp_dallas_lite_css_generator()); //
 	if (is_singular() && comments_open() && get_option('thread_comments'))
 		{
 		wp_enqueue_script('comment-reply');
