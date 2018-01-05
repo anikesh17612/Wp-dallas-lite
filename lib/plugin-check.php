7aa89b5f48732b486102c0b5215517183ffb6c07
Siddhatha Agarwal <siddhartha10jun@gmail.com>
2018-01-06 00:26:04 +0530

    add plugin code to dismiss notice issues solve

--- lib/plugin-check.php
+++ lib/plugin-check.php
@@ -8,32 +8,21 @@
 defined('ABSPATH') or die('No script kiddies please!');
 include_once (ABSPATH . 'wp-admin/includes/plugin.php');
 
-add_action('admin_notices', 'my_theme_dependencies');
+function wp_dallas_lite_plugin_notice() {
+    $user_id = get_current_user_id();
+    if ( !get_user_meta( $user_id, 'wp_dallas_lite_notice_dismissed' ) ) {
+		if( !function_exists('wps_get_mailing_services') || !class_exists('wpt_widget')){
+		echo '<div id="message" class="notice notice-success is-dismissible"><p>' . __( 'This theme recommends the following plugins:', 'wp_dallas_lite' ) .' <a href="'.admin_url().'plugin-install.php?tab=plugin-information&plugin=wp-subscribe&TB_iframe=true&width=640&height=500" class="thickbox">WP Subscribe</a>   <a href="'.admin_url().'plugin-install.php?tab=plugin-information&plugin=wp-tab-widget&TB_iframe=true&width=640&height=500" class="thickbox">WP Tab Widget</a></p>
+		<a href="' . esc_url( wp_nonce_url( add_query_arg( 'tgmpa-dismiss', 'dismiss_admin_notices' ), 'tgmpa-dismiss-' . get_current_user_id() ) ) . '" class="dismiss-notice" target="_parent">Dismiss this notice</a></p></div>';		
+		}	
 
-function my_theme_dependencies() {
- if( !function_exists('wps_get_mailing_services') || !class_exists('wpt_widget')){
-    echo '<div id="message" class="notice notice-success is-dismissible"><p>' . __( 'This theme recommends the following plugins:', 'wp_dallas_lite' ) .' <a href="'.admin_url().'plugin-install.php?tab=plugin-information&plugin=wp-subscribe&TB_iframe=true&width=640&height=500" class="thickbox">WP Subscribe</a>   <a href="'.admin_url().'plugin-install.php?tab=plugin-information&plugin=wp-tab-widget&TB_iframe=true&width=640&height=500" class="thickbox">WP Tab Widget</a></p><p><a class="is-dismissible " href="'.wp_dallas_lite_activation_link('#', 'activate').'">
-        Dismiss this notice </a></p></div>';
- }
- else{
-      
- }
- 
-}
-
-function wp_dallas_lite_activation_link($plugin, $action = 'activate')
-	{
-	if (strpos($plugin, '/'))
-		{
-		$plugin = str_replace('\/', '%2F', $plugin);
-		}
-
-	$url = sprintf(admin_url('plugins.php?action=' . $action . '&plugin=%s&plugin_status=all&paged=1&s') , $plugin);
-	$_REQUEST['plugin'] = $plugin;
-	if ($_REQUEST['plugin'])
-		{
-		$url = wp_nonce_url($url, $action . '-plugin_' . $plugin);
-		}
-
-	return $url;
 	}
+}
+add_action( 'admin_notices', 'wp_dallas_lite_plugin_notice' );
+
+function wp_dallas_lite_notice_dismissed() {
+    $user_id = get_current_user_id();
+    if ( isset( $_GET['tgmpa-dismiss'] ) )
+        add_user_meta( $user_id, 'wp_dallas_lite_notice_dismissed', 'true', true );
+}
+add_action( 'admin_init', 'wp_dallas_lite_notice_dismissed' );
