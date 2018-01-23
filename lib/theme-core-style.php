c094761971d28ee165c87f262e58013f7bbb9004
roshanjajoriya <roshan.jajoriya@joomdev.com>
2018-01-23 23:53:39 +0530

    some crrection input fields

--- lib/theme-core-style.php
+++ lib/theme-core-style.php
@@ -51,7 +51,7 @@ if (!function_exists('wp_dallas_lite_css_generator'))
 		$hover_color = get_theme_mod('hover_color', '#e6ac00');
 		if ($hover_color)
 			{
-			$output.= 'a:hover, #top-menu > li > a:hover, #site-navigation ul#primary-menu > li > a:hover, .widget ul li a:hover, #bottom-section ul.menu > li > a:hover, #bottom-section ul.menu > li > a:focus, #bottom-section ul > li > a:hover, .post-content-wrapper-controller:hover, .post-content-wrapper-controller .fa.pull-left:hover, .post-content-wrapper-controller .fa.pull-right:hover , .widget.widget_rss ul li a,.social-share a:hover{ color: ' . esc_attr($hover_color) . '; }';
+			$output.= 'a:hover, #footer-section a:hover, #top-menu > li > a:hover, #site-navigation ul#primary-menu > li > a:hover, .widget ul li a:hover, #bottom-section ul.menu > li > a:hover, #bottom-section ul.menu > li > a:focus, #bottom-section ul > li > a:hover, .post-content-wrapper-controller:hover, .post-content-wrapper-controller .fa.pull-left:hover, .post-content-wrapper-controller .fa.pull-right:hover , .widget.widget_rss ul li a,.social-share a:hover{ color: ' . esc_attr($hover_color) . '; }';
 			$output.= '.error-page-inner a.btn.btn-primary.btn-lg:hover{ background-color: ' . esc_attr($hover_color) . '; }';
 			$output.= '.woocommerce a.button:hover{ border-color: ' . esc_attr($hover_color) . ' }';
 			}
@@ -208,12 +208,12 @@ if (!function_exists('wp_dallas_lite_css_generator'))
 
 		if (get_theme_mod('button_bg_color', '#222533'))
 			{
-			$output.= '.mc4wp-form-fields input[type=submit], form button, form input[type="button"], form input[type="reset"], form input[type="submit"] , #comments .form-submit input.submit, a btn, .btn, .btn-info, .btn-primary, .demo-four .mc4wp-form-fields input[type=submit], .common-menu-wrap .nav>li.online-booking-button a, .error-page-inner a.btn.btn-primary.btn-lg,.btn.btn-primary, .package-list-button,
+			$output.= '.mc4wp-form-fields input[type=submit], form button,#wp-subscribe.wp-subscribe-wrap input.submit, form input[type="button"], form input[type="reset"], form input[type="submit"] , #comments .form-submit input.submit, a btn, .btn, .btn-info, .btn-primary, .demo-four .mc4wp-form-fields input[type=submit], .common-menu-wrap .nav>li.online-booking-button a, .error-page-inner a.btn.btn-primary.btn-lg,.btn.btn-primary, .package-list-button,
       .contact-submit input[type=submit],.form-submit input[type=submit]{ background-color: ' . esc_attr(get_theme_mod('button_bg_color', '#222533')) . '  ; border-color: ' . esc_attr(get_theme_mod('button_bg_color', '#222533')) . '; color: ' . esc_attr(get_theme_mod('button_text_color', '#fff')) . ';border-radius: 4px; }';
 			}
 		if (get_theme_mod('button_hover_bg_color', '#363b52'))
 			{
-			$output.= '.mc4wp-form-fields input[type=submit]:hover, form button:hover, form input[type="button"]:hover, form input[type="reset"]:hover, form input[type="submit"]:hover, #comments .form-submit input.submit:hover, .btn-info:hover, .btn-info:focus, .btn-info:active:focus, .demo-four .mc4wp-form-fields input[type=submit]:hover, .common-menu-wrap .nav>li.online-booking-button a:hover, .error-page-inner a.btn.btn-primary.btn-lg:hover,.btn.btn-primary:hover, .package-list-button:hover, .contact-submit input[type=submit]:hover,.form-submit input[type=submit]:hover{ background-color: ' . esc_attr(get_theme_mod('button_hover_bg_color', '#363b52')) . '; border-color: ' . esc_attr(get_theme_mod('button_hover_bg_color', '#363b52')) . '; color: ' . esc_attr(get_theme_mod('button_hover_text_color', '#ffffff')) . '}';
+			$output.= '.mc4wp-form-fields input[type=submit]:hover,#wp-subscribe.wp-subscribe-wrap input.submit:hover, form button:hover, form input[type="button"]:hover, form input[type="reset"]:hover, form input[type="submit"]:hover, #comments .form-submit input.submit:hover, .btn-info:hover, .btn-info:focus, .btn-info:active:focus, .demo-four .mc4wp-form-fields input[type=submit]:hover, .common-menu-wrap .nav>li.online-booking-button a:hover, .error-page-inner a.btn.btn-primary.btn-lg:hover,.btn.btn-primary:hover, .package-list-button:hover, .contact-submit input[type=submit]:hover,.form-submit input[type=submit]:hover{ background-color: ' . esc_attr(get_theme_mod('button_hover_bg_color', '#363b52')) . '; border-color: ' . esc_attr(get_theme_mod('button_hover_bg_color', '#363b52')) . '; color: ' . esc_attr(get_theme_mod('button_hover_text_color', '#ffffff')) . '}';
 			}
 
 		$output.= '.subtitle-cover:before{background:' . get_theme_mod('sub_header_overlayer_color', 'rgba(0, 0, 0, 0.5)') . ';}';
