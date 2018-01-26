44724c569ee477bb6c320bf5e0471e18049b4d54
siddhartha agarwal <siddhartha10jun@gmail.com>
2018-01-26 22:12:27 +0530

    change condition

--- single.php
+++ single.php
@@ -123,11 +123,11 @@ endwhile; // End of the loop.
 
 // for use in the loop, list 5 post titles related to first tag on current post
 
-$tags = wp_get_post_tags($post->ID);
+//$tags = wp_get_post_tags($post->ID);
 
-if ($tags != '')
+if (wp_get_post_tags($post->ID) != '')
 	{
-	$first_tag = $tags[0]->term_id;
+	$first_tag = wp_get_post_tags($post->ID,'term_id',true);
 	$args = array(
 		'tag__in' => array(
 			$first_tag
